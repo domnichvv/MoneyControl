e6731d5aeac98e68b90d8fa7eb10ccce18e19137
VLAD-PK\Vlad <vlad_domnich@mail.ru>
2017-02-22 13:50:45 +0200

    create databese read

--- app/src/main/java/com/domnich/vlad/moneycontrol/controller/MainActivity.java
+++ app/src/main/java/com/domnich/vlad/moneycontrol/controller/MainActivity.java
@@ -1,13 +1,17 @@
 package com.domnich.vlad.moneycontrol.controller;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import com.domnich.vlad.moneycontrol.R;
+import com.domnich.vlad.moneycontrol.model.database.DBHelper;
+import com.domnich.vlad.moneycontrol.model.entities.User;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -16,10 +20,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final int REQUEST_CODE = 1;
 
+    User user;
+    DBHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.activity_main);
 
         edTextLogin = (EditText) findViewById(R.id.edTextLogin);
         edTextPass = (EditText) findViewById(R.id.edTextPass);
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         buttonSignIn.setOnClickListener(this);
         buttonSignUp.setOnClickListener(this);
+
+        dbHelper = new DBHelper(this);
     }
 
     @Override
@@ -39,10 +48,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String login = edTextLogin.getText().toString();
         String password = edTextPass.getText().toString();
 
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
         switch (v.getId()){
             case R.id.buttonSignIn:
-                intent = new Intent(this, MoneyActivity.class);
-                startActivity(intent);
+                //checking fields empty or not
+                if(login.equals("") || password.equals("")){
+                    Toast.makeText(this, R.string.toast_empty_fields, Toast.LENGTH_SHORT).show();
+                } else {
+                    Cursor cursor = database.query(DBHelper.TABLE_USERS, null, null, null, null, null, null);
+
+                    if(cursor.moveToFirst()){
+                        int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+                        int loginIndex = cursor.getColumnIndex(DBHelper.KEY_LOGIN);
+                        int passIndex = cursor.getColumnIndex(DBHelper.KEY_PASS);
+                        do{
+                            if(login.equals(cursor.getString(loginIndex)) & password.equals(cursor.getString(passIndex))){
+                                user = new User(cursor.getInt(idIndex), login, password);
+                                Toast.makeText(this, R.string.toast_success_sign_in, Toast.LENGTH_SHORT).show();
+                                intent = new Intent(this, MoneyActivity.class);
+                                startActivity(intent);
+                            } else if(cursor.isLast()){
+                                Toast.makeText(this, R.string.toast_wrong_sign_in, Toast.LENGTH_SHORT).show();
+                            }
+                        } while (cursor.moveToNext());
+                    } else {
+                        Toast.makeText(this, R.string.toast_wrong_sign_in, Toast.LENGTH_SHORT).show();
+                    }
+                    cursor.close();
+                    dbHelper.close();
+                }
                 break;
             case R.id.buttonSignUp:
                 intent = new Intent(this, SignUpActivity.class);
