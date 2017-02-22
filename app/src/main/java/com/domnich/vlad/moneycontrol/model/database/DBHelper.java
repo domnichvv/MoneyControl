cb134b357795cb3b18b6746292d03199a9e20601
VLAD-PK\Vlad <vlad_domnich@mail.ru>
2017-02-22 13:51:40 +0200

    create DBHelper

--- app/src/main/java/com/domnich/vlad/moneycontrol/model/database/DBHelper.java
+++ app/src/main/java/com/domnich/vlad/moneycontrol/model/database/DBHelper.java
@@ -1,8 +1,37 @@
 package com.domnich.vlad.moneycontrol.model.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Vlad on 22.02.2017.
  */
 
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "moneyDb";
+    public static final String TABLE_USERS = "users";
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_LOGIN = "login";
+    public static final String KEY_PASS = "password";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_USERS + "(" + KEY_ID + " integer primary key,"
+                + KEY_LOGIN + " text," + KEY_PASS + " text" + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exist " + TABLE_USERS);
+
+        onCreate(db);
+    }
 }
